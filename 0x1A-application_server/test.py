4c3d19ee5cd53122a0c5af157b6a123805d71354
jemima <ashbeca16@gmail.com>
2024-01-24 15:05:07 +0200

    adding files

---
+++ 0x1A-application_server/test.py
@@ -0,0 +1,16 @@
+#setting up the ssh agent
+
+eval $("ssh-agent")
+
+#path to identifyfile
+
+ssh-add ~/.ssh/name of the file
+
+#username and IP Address
+
+ssh ubuntu@your IP Adress
+
+
+name of the file: file you save your ssh key
+
+your IP Adress: your server IP
